1a2e0f683c4c43df0f51be5bbd2e55ff1d7a1d09
Marcin Kaszkowiak <iboy@mbpRetina.local>
2017-11-21 00:27:09 +0100

    kurwa

--- skarbonka/checker.sh
+++ skarbonka/checker.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-for i in {1..999} 
-do
-    #./main <'./ska_srednie/in/in$i.in' >out
 
+i=0
+while [ $i -le 999 ]
+{
+    ./main <'./ska_srednie/in/in$i.in' >out
+ 
     if [ diff out ./ska_srednie/out/out"$i".out -ne ""]
     then
         echo $"WA"
     else
         echo $"OK"
     fi
-done
 
+    i=$[i+1]
+}
+
+exit 1
